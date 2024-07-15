f8f6b572d0479e2bd47e99d6758e3de5c9efbd76
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-07-15 23:58:42 +0530

    complete

--- src/App.java
+++ src/App.java
@@ -8,7 +8,6 @@ import javafx.stage.Stage;
 
 public class App extends Application {
     public static void main(String[] args) throws Exception {
-        // System.out.println("Hello, World!");
         launch(args);
     }
 
